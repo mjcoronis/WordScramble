763e228c0306edc94f8456c4efb2ff2178391b9a
mjcoronis <124647054+mjcoronis@users.noreply.github.com>
2023-06-02 22:37:47 -0700

    Add files via upload

---
+++ Word Scramble/words.js
@@ -0,0 +1,45 @@
+let words = [
+    {
+        word: "outside",
+        hint: "left side hitter"
+    }, {
+        word: "middle",
+        hint: "tallest on the team"
+    }, {
+        word: "number",
+        hint: "everyone has one"
+    }, {
+        word: "spike", 
+        hint: "hitting the ball"
+    }, {
+        word: "right",
+        hint: "opposite hitter"
+    }, {
+        word: "libero",
+        hint: "designated passed"
+    }, {
+        word: "dig",
+        hint: "a pass"
+    }, {
+        word: "uniform",
+        hint: "jersey"
+    }, {
+        word: "coach",
+        hint: "makes the plays"
+    }, {
+        word: "setter",
+        hint: "soft hands"
+    }, {
+        word: "serve",
+        hint: "start the game"
+    }, {
+        word: "league",
+        hint: "different areas have them"
+    }, {
+        word: "block",
+        hint: "not letting the ball pass"
+    }, {
+        word: "referee",
+        hint: "makes the calls"
+    }
+]
